98291992e1af2757cc73b91b71149905d706f25a
Clement_Alpha <956163372@qq.com>
2019-01-31 10:38:57 +0800

    添加校验

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import PropTypes from 'prop-types'
 
 class TodoItem extends Component {
     constructor(props) {
@@ -6,8 +7,8 @@ class TodoItem extends Component {
         this.handleClick = this.handleClick.bind(this);
     }
     render() {
-        const { content } = this.props;
-        return <div onClick={this.handleClick}>{ content }</div>
+        const { content, test } = this.props;
+        return <div onClick={this.handleClick}>{test}-{ content }</div>
     }
 
     handleClick() {
@@ -15,5 +16,15 @@ class TodoItem extends Component {
         deleteItem(index)
     }
 }
+TodoItem.propTypes = {
+    test: PropTypes.string.isRequired,
+    content: PropTypes.string.isRequired,
+    deleteItem: PropTypes.func,
+    index: PropTypes.number
+}
 
+TodoItem.defaultProps = {
+    test: 'hello world'
+
+}
 export default TodoItem;
