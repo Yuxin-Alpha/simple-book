20e959918b08462f96bd6c9cac6dc5ec88094adc
Clement_Alpha <956163372@qq.com>
2019-04-18 12:44:08 +0800

    重写

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component, Fragment } from 'react';
+import { Globalstyle } from './style'
+class App extends Component {
+  render() {
+    return (
+      <Fragment>
+        <Globalstyle />
+        <div>simple book
+        </div>
+      </Fragment>
+    );
+  }
+}
+
+export default App;
