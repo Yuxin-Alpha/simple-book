1d0cd7906a49b4bda4f5cefa707870898dd2d483
Clement_Alpha <956163372@qq.com>
2019-04-19 10:28:33 +0800

    store添加

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,19 @@
 import React, { Component, Fragment } from 'react';
 import { Globalstyle } from './style'
+import { Provider } from 'react-redux'
 import { IconFont } from './static/iconfont/iconfont'
 import Header from './common/header'
+import store from './store'
+
 class App extends Component {
   render() {
     return (
       <Fragment>
-        <Globalstyle />
-        <IconFont />
-        <Header/>
+        <Provider store={store}>
+          <Globalstyle />
+          <IconFont />
+          <Header/>
+        </Provider>
       </Fragment>
     );
   }
--- client/src/common/header/index.js
+++ client/src/common/header/index.js
@@ -1,63 +1,65 @@
-import React, { Component } from 'react'
+import React from 'react'
+import { connect } from 'react-redux'
 import { HeaderWrapper, Logo, Nav, NavItem, NavSearch, Addition, Button, SearchWrapper } from "./style";
 import { CSSTransition } from 'react-transition-group'
 
-
-class Header extends  Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      focused: false
-    }
-    this.handleInputFocus = this.handleInputFocus.bind(this)
-    this.handleInputBlur = this.handleInputBlur.bind(this)
-  }
-  render() {
-    return (
-      <HeaderWrapper>
-        <Logo />
-        <Nav>
-          <NavItem className="left active">首页</NavItem>
-          <NavItem className="left">下载APP</NavItem>
-          <NavItem className="right">
-            <i className="iconfont">&#xe636;</i>
-          </NavItem>
-          <NavItem className="right">登录</NavItem>
-          <SearchWrapper>
-            <CSSTransition
-              timeout={300}
-              in={this.state.focused}
-              classNames="slide"
+const Header = (props) => {
+  return (
+    <HeaderWrapper>
+      <Logo />
+      <Nav>
+        <NavItem className="left active">首页</NavItem>
+        <NavItem className="left">下载APP</NavItem>
+        <NavItem className="right">
+          <i className="iconfont">&#xe636;</i>
+        </NavItem>
+        <NavItem className="right">登录</NavItem>
+        <SearchWrapper>
+          <CSSTransition
+            timeout={300}
+            in={props.focused}
+            classNames="slide"
+          >
+            <NavSearch
+              className={props.focused ? 'focused' : ''}
+              onFocus={props.handleInputFocus}
+              onBlur={props.handleInputBlur}
             >
-              <NavSearch
-                className={this.state.focused ? 'focused' : ''}
-                onFocus={this.handleInputFocus}
-                onBlur={this.handleInputBlur}
-              >
-              </NavSearch>
-            </CSSTransition>
-            <i className={this.state.focused ? 'focused iconfont' : 'iconfont'}>&#xe62d;</i>
-          </SearchWrapper>
-          
-          <Addition>
-            <Button className="article">
-              <i className="iconfont">&#xe616;</i>写文章
-            </Button>
-            <Button className="reg">注册</Button>
-          </Addition>
-        </Nav>
-      </HeaderWrapper>
-    )
-  }
-  handleInputFocus () {
-    this.setState({
-      focused: true
-    })
-  }
-  handleInputBlur () {
-    this.setState({
-      focused: false
-    })
+            </NavSearch>
+          </CSSTransition>
+          <i className={props.focused ? 'focused iconfont' : 'iconfont'}>&#xe62d;</i>
+        </SearchWrapper>
+      
+        <Addition>
+          <Button className="article">
+            <i className="iconfont">&#xe616;</i>写文章
+          </Button>
+          <Button className="reg">注册</Button>
+        </Addition>
+      </Nav>
+    </HeaderWrapper>
+  )
+}
+
+const mapStateToProps = (state) => {
+  return {
+    focused: state.focused
   }
 }
-export default Header;
+const mapDispatchToProps = (dispatch) => {
+  return {
+    handleInputFocus() {
+      const action = {
+        type: 'search_focus'
+      }
+      dispatch(action)
+    },
+    handleInputBlur() {
+      const action = {
+        type: 'search_blur'
+      };
+      dispatch(action)
+    }
+  }
+}
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
